c7997043df114f7028b0fbad697101debd427906
Shakil Ahmmed <shakilfci461@gmail.com>
2021-11-15 09:38:31 +0600

    Login

--- routes/index.ts
+++ routes/index.ts
@@ -1,14 +1,18 @@
-import express, {Router} from 'express';
+import express, { Router } from 'express';
 import exampleController from '../controllers/exampleController';
 import userController from '../controllers/userController';
 
 let router: Router = express.Router();
 
 router.get('/example', exampleController.index).post('/example', exampleController.store);
-router.get('/users', userController.index).post('/users', userController.validation, userController.store);
+router
+  .get('/users', userController.index)
+  .post('/users', userController.validation, userController.store);
+
+router.post('/login', userController.loginValidation, userController.login);
 
 router
-    .get('/example/:id', exampleController.show)
-    .delete('/example/:id', exampleController.destroy);
+  .get('/example/:id', exampleController.show)
+  .delete('/example/:id', exampleController.destroy);
 
 export default router;
